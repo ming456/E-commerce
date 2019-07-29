e1e21006a072e53001ab50668b6bf17fdb21d011
guoge <ggldwyyx@163.com>
2019-07-29 16:04:20 +0800

    解决同步问题

--- src/main/java/com/neu/dao/AdminMapper.java
+++ src/main/java/com/neu/dao/AdminMapper.java
@@ -3,8 +3,24 @@ package com.neu.dao;
 import com.neu.bean.Admin;
 import org.apache.ibatis.annotations.Param;
 
-public interface AdminMapper {
-   int insertAdmin(Admin admin);
+import java.util.List;
 
-    Admin findAdmin(@Param("username") String username, @Param("password") String password);
+public interface AdminMapper {
+
+   //添加管理员
+   int insertAdmin(Admin admin);
+   //删除管理员
+   int deleteAdmin(int id);
+   //修改管理员权限
+   int updateAdminAccess(Admin admin);
+   //查找所有管理员
+   List<Admin> selectAdminAll();
+   //根据编号查找管理员
+   Admin selectAdminById(int id);
+   //根据名字模糊查询查找管理员
+   Admin selectAdminByName(String superName);
+   //根据名字模糊查询查找管理员
+   List<Admin> selectAdminLikeName(String superName);
+
+   Admin selectAdmin(String supername,String password);
 }
--- src/main/java/com/neu/service/AdminService.java
+++ src/main/java/com/neu/service/AdminService.java
@@ -2,8 +2,25 @@ package com.neu.service;
 
 import com.neu.bean.Admin;
 
-public interface AdminService {
-    boolean addAdmin(Admin admin);
+import java.util.List;
 
-    Admin findAdmin(String username, String password);
+public interface AdminService {
+    //添加管理员
+    boolean addAdmin(Admin admin);
+    //删除管理员
+    boolean removeAdmin(int id);
+    //修改管理员权限
+    boolean modifyAdminAccess(Admin admin);
+    //查找所有管理员
+    List<Admin> findAdminAll();
+    //根据编号查找管理员
+    Admin findAdminById(int id);
+    //根据名字模糊查询查找管理员
+    List<Admin> findAdminLikeName(String superName);
+    //根据名字查询查找管理员
+    Admin findAdminByName(String superName);
+    //什么管理员
+    int whatAdmin(String superName);
+    //name，psw找找管理员
+    Admin findAdmin(String username,String password);
 }
